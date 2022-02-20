8c5fcdff4078bde8aeba202e757b65e92a83b48e
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-20 12:22:46 -0800

    add a user verification function

--- run.py
+++ run.py
@@ -13,3 +13,21 @@ def saveuser(user):
     Save new user account
     """
     user.save_user()
+
+def deleteuser(user):
+    """
+    Delete user account
+    """
+    user.delete_user()
+
+def verifyuser(user_name):
+    """
+    Verify user using the username
+    """
+    return User.verify_username(user_name)
+
+def verifyusernameandpasswd(user_name, user_passwd):
+    """
+    Verify the user and password exists in the given lists
+    """
+    return User.verify_username_and_password(user_name, user_passwd)
