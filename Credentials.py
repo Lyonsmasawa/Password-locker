69eae292e6b56d9afcbc6def5fc1c73aa4e7cf46
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-20 10:51:06 -0800

    create a save function

--- Credentials.py
+++ Credentials.py
@@ -3,7 +3,7 @@ class Credentials:
     Create new instance of credentials
     """
     crendetials_list = []
-    
+
     def __init__(self, platform, username, password):
         self.platform = platform
         self.username = username
@@ -11,5 +11,5 @@ class Credentials:
 
     def save_cred(self):
         """
-        
+        Saves the new credentials to the credentials list
         """
