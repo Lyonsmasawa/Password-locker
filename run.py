9d5203e629d2750a22fdce30f6bf3497451b57e9
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-20 12:13:06 -0800

    add a function to create and save new user

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
 from User import User
 from Credentials import Credentials
+
+def createuser(user_name, user_passwd):
+    """
+    Create user account
+    """
+    new_user = User(user_name, user_passwd)
+    return new_user
+
+def saveuser(user):
+    """
+    Save new user account
+    """
+    user.save_user()
