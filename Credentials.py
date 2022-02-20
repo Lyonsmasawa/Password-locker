c08445d1842f0b6ebc38cd2cb1a548b845518086
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-20 12:35:00 -0800

    add a function to check if a credential exists

--- Credentials.py
+++ Credentials.py
@@ -39,3 +39,12 @@ class Credentials:
         for cred in cls.credentials_list:
             if cred.platform == platform:
                 return cred
+
+    def cred_exist(cls, platform):
+        """
+        Checks if a credential exists
+        """
+        for cred in cls.credentials_list:
+            if cred.platform == platform:
+                return True
+        return False
