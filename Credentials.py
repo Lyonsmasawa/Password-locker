5ba1bec3759093effb8497dc2ee59329fcf7314a
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-02-20 10:48:37 -0800

    Create a constructor

--- Credentials.py
+++ Credentials.py
@@ -2,3 +2,12 @@ class Credentials:
     """
     Create new instance of credentials
     """
+    def __init__(self, platform, username, password):
+        self.platform = platform
+        self.username = username
+        self.password = password
+
+    def save_cred(self):
+        """
+        
+        """
